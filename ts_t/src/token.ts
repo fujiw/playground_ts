00fd766227b58c3f823b51759df92082475b8a4c
fujiw <fujiw10@gmail.com>
2020-11-09 23:52:54 +0900

    add token

--- ts_t/src/token.ts
+++ ts_t/src/token.ts
@@ -1,7 +1,17 @@
+enum TokenType {
+  ASSIGN = '=',
+  PLUS = '+',
+  MINUS = '-',
+  BANG = '!',
+  ASTERISK = '*',
+}
+
 export class Token {
   char: string
+  token_type: TokenType
 
   constructor(char: string) {
     this.char = char
+    this.token_type = TokenType.PLUS
   }
 }
